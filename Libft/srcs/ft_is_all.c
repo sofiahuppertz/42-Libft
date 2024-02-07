5b3c5c196f664e4fe98c94511226a5ff8de4249c
Sofia Huppertz <sofia.huppertz@gmail.com>
2024-02-07 13:01:27 +0100

    ft_is_all

--- Libft/srcs/ft_is_all.c
+++ Libft/srcs/ft_is_all.c
@@ -6,7 +6,7 @@
 /*   By: shuppert <shuppert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 13:35:15 by shuppert          #+#    #+#             */
-/*   Updated: 2023/11/28 13:35:21 by shuppert         ###   ########.fr       */
+/*   Updated: 2024/02/07 13:01:10 by shuppert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_is_all(const char *str, int (*f)(int))
 {
+	if (str == NULL)
+		return (1);
 	while (*str)
 	{
 		if (!f(*str))
